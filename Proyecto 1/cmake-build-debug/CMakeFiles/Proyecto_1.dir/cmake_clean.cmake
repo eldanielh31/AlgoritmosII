a2db1c5e0a6000aa448cdf0e3aba1346a4eb2b3e
Daniel <danbg31@gmail.com>
2020-02-24 11:09:15 -0600

    Prueba conexion

---
+++ Proyecto 1/cmake-build-debug/CMakeFiles/Proyecto_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto_1.dir/main.cpp.o"
+  "Proyecto_1"
+  "Proyecto_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
