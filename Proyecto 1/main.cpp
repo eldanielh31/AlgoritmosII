514f7ea255a5e86bc1c3b397425fd673b9aff95f
Daniel Desktop <danbg31@gmail.com>
2020-02-24 18:26:01 -0600

    Empezando servidor

--- Proyecto 1/main.cpp
+++ Proyecto 1/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
-
-using namespace std;
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <stdio.h>
 
 int main() {
-    cout << "Hello, World!" << endl;
+
+    struct sockaddr_in direccionServidor;
+    direccionServidor.sin_family = AF_INET;
+    direccionServidor.sin_addr.s_addr = INADDR_ANY;
+    direccionServidor.sin_port = htons(8080);
+
+    int servidor = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (bind (servidor, (void*) &direccionServidor, sizeof(direccionServidor)) != 0 )
+
+
+
     return 0;
 }
